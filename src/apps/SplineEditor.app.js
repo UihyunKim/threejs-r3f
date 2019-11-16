03abe120957c7a320aa527a1336a2d72170cdd50
Uihyun KIM <stringee@gmail.com>
2019-11-16 16:44:04 +0100

    update

--- src/apps/SplineEditor.app.js
+++ src/apps/SplineEditor.app.js
@@ -1,11 +1,28 @@
 import React from "react";
+import * as THREE from "three";
+import { Canvas } from "react-three-fiber";
 
-function SplineEditorApp() {
+/** GUI */
+import Stats from "three/examples/jsm/libs/stats.module";
+import { GUI } from "three/examples/jsm/libs/dat.gui.module";
+
+/** CONTROLS */
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { DragControls } from "three/examples/jsm/controls/DragControls";
+import { TransformControls } from "three/examples/jsm/controls/TransformControls";
+
+const SplineEditorApp = () => {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <Canvas
+      camera={{
+        position: [0, 250, 1000],
+        fov: 70,
+        aspect: window.innerWidth / window.innerHeight,
+        near: 1,
+        far: 10000
+      }}
+    ></Canvas>
   );
-}
+};
 
 export default SplineEditorApp;
